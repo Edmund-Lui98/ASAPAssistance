8b8f0e6d334ee1e924d7e0d1857971eebb824666
EdmundLuii <edmund.lui1998@gmail.com>
2019-11-23 10:07:04 -0500

    Added firebase authentication for login activity

---
+++ app/src/main/java/com/example/asapassistance/LoginActivity.java
@@ -0,0 +1,59 @@
+package com.example.asapassistance;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    ImageButton btnLogin;
+    EditText username;
+    EditText password;
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.usernameInput);
+        password = findViewById(R.id.passwordInput);
+        btnLogin = findViewById(R.id.loginBtn);
+    }
+
+    public void onClickLogin(View view) {
+        mAuth = FirebaseAuth.getInstance();
+
+        if (username.length() > 0 && password.length() > 0) {
+            mAuth.signInWithEmailAndPassword(username.getText().toString(), password.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        logIn();
+                    } else {
+                        Toast toast = Toast.makeText(getApplicationContext(), "Invalid Email or Password. Please try again.", Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                }
+            });
+        } else {
+            Toast toast_2 = Toast.makeText(getApplicationContext(), "Please enter username and/or password", Toast.LENGTH_SHORT);
+            toast_2.show();
+        }
+    }
+    public void logIn() {
+        Intent myIntent = new Intent(this, MainActivity.class);
+        this.startActivity(myIntent);
+    }
+}
--- app/src/main/java/com/example/asapassistance/MainActivity.java
+++ app/src/main/java/com/example/asapassistance/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.asapassistance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
