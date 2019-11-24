fda0d856e84c999c36378c73e755503387e818b0
EdmundLuii <edmund.lui1998@gmail.com>
2019-11-23 22:02:51 -0500

    finished call and sms stuff. Working on UI now

--- app/src/main/java/com/example/asapassistance/MainActivity.java
+++ app/src/main/java/com/example/asapassistance/MainActivity.java
@@ -9,9 +9,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
@@ -20,10 +20,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Scanner;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
-
 public class MainActivity extends AppCompatActivity{
-    static String URLstring = "https://api.zang.io/v2/Accounts/AC777c3e32b672b6782bfe4d798a4e6800/SMS/Messages.json";
 
     TextView result;
 
@@ -37,45 +34,22 @@ public class MainActivity extends AppCompatActivity{
         new AsyncClassGet().execute();
     }
 
-    public void onCLicksendSMS(View view) throws IOException {
+    public void onClicksendSMS(View view) throws IOException {
         Log.i("ASAPAssistance", "Button pressed");
 
         new AsyncClassPost().execute();
 
     }
-    /*
-    public static String sendSMS(URL url, HttpURLConnection connection) throws IOException {
-        String ret;
+    public void onClickCall(View view) throws IOException {
+        Log.i("ASAPAssistance", "called");
 
-        Log.i("ASAPAssistance", "Send sms function");
-
-        try {
-            InputStream in = connection.getInputStream();
-            Log.i("ASAPAssistance", "after input stream");
-
-            Scanner scanner = new Scanner(in);
-            scanner.useDelimiter("\\A");
-
-            boolean hasInput = scanner.hasNext();
-            if (hasInput) {
-                ret =  scanner.next();
-            } else {
-                ret =  null;
-            }
-            return ret;
-        } catch (Exception e) {
-            .setText(e.toString());
-        } finally {
-            connection.disconnect();
-        }
-        ret =  "hello";
-        return ret;
+        new AsyncClassCall().execute();
     }
-
-     */
 
     private class AsyncClassGet extends AsyncTask<Void, Void, String> {
         String ret;
+        String URLstring = "https://api.zang.io/v2/Accounts/AC777c3e32b672b6782bfe4d798a4e6800/SMS/Messages.json";
+
 
         @Override
         protected String doInBackground(Void... voids) {
@@ -126,13 +100,18 @@ public class MainActivity extends AppCompatActivity{
             super.onPostExecute(s);
 
             result.setText(ret);
-            Log.i("MainActivity","Is it finished???");
+            Log.i("MainActivity","Message recieved");
         }
     }
 
     private class AsyncClassPost extends AsyncTask<Void, Void, String> {
-        String ret;
+        String URLstring = "https://api.zang.io/v2/Accounts/AC777c3e32b672b6782bfe4d798a4e6800/SMS/Messages.json";
 
+        String ret;
+        int res;
+        String urlParameters  = "To=+16477068738&From=+19412004022&Body=jkjk! from UWO :) (dont reply to this) ";
+        byte[] postData       = urlParameters.getBytes( StandardCharsets.UTF_8 );
+        int    postDataLength = postData.length;
         @Override
         protected String doInBackground(Void... voids) {
             String x = "AC777c3e32b672b6782bfe4d798a4e6800" + ":" + "5fce2bddc8744fac940b51d893c7a385";
@@ -144,13 +123,36 @@ public class MainActivity extends AppCompatActivity{
                 connection.setRequestMethod("POST");
                 connection.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
                 connection.setRequestProperty( "charset", "utf-8");
-                connection.setRequestProperty( "Content-Length", "To=6477068738&From=9412004022&Body=This is an SMS sent from Edmund");
+                connection.setRequestProperty( "Content-Length", Integer.toString(postDataLength));
+                String encoded = Base64.getEncoder().encodeToString((x).getBytes());  //Java 8
+                connection.setRequestProperty("Authorization", "Basic " + encoded);
                 connection.setUseCaches( false );
+                try( DataOutputStream wr = new DataOutputStream( connection.getOutputStream())) {
+                    wr.write( postData );
+                }
+                res = connection.getResponseCode();
+
 
                 Log.i("ASAPAssistance", "Send sms function");
+                try {
+                    InputStream in = connection.getInputStream();
+                    Log.i("ASAPAssistance", "after input stream");
 
-                connection.disconnect();
+                    Scanner scanner = new Scanner(in);
+                    scanner.useDelimiter("\\A");
 
+                    boolean hasInput = scanner.hasNext();
+                    if (hasInput) {
+                        ret =  scanner.next();
+                    } else {
+                        ret = null;
+                    }
+                    return ret;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    connection.disconnect();
+                }
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             } catch (ProtocolException e) {
@@ -160,6 +162,81 @@ public class MainActivity extends AppCompatActivity{
             }
 
             return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            result.setText(Integer.toString(res));
+            Log.i("MainActivity","Message sent!");
+        }
+    }
+    private class AsyncClassCall extends AsyncTask<Void, Void, String> {
+        String URLstring = "https://api.zang.io/v2/Accounts/AC777c3e32b672b6782bfe4d798a4e6800/Calls.json";
+        String ret;
+        int res;
+        String urlParameters  = "From=+19412004022&To=+16477068738&Url=http://zang.io/ivr/welcome/call";
+        byte[] postData       = urlParameters.getBytes( StandardCharsets.UTF_8 );
+        int    postDataLength = postData.length;
+        @Override
+        protected String doInBackground(Void... voids) {
+            String x = "AC777c3e32b672b6782bfe4d798a4e6800" + ":" + "5fce2bddc8744fac940b51d893c7a385";
+            try {
+                URL url = new URL(URLstring);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setDoOutput(true);
+                connection.setInstanceFollowRedirects( false );
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
+                connection.setRequestProperty( "charset", "utf-8");
+                connection.setRequestProperty( "Content-Length", Integer.toString(postDataLength));
+                String encoded = Base64.getEncoder().encodeToString((x).getBytes());  //Java 8
+                connection.setRequestProperty("Authorization", "Basic " + encoded);
+                connection.setUseCaches( false );
+                try( DataOutputStream wr = new DataOutputStream( connection.getOutputStream())) {
+                    wr.write( postData );
+                }
+                res = connection.getResponseCode();
+
+
+                Log.i("ASAPAssistance", "Send sms function");
+                try {
+                    InputStream in = connection.getInputStream();
+                    Log.i("ASAPAssistance", "after input stream");
+
+                    Scanner scanner = new Scanner(in);
+                    scanner.useDelimiter("\\A");
+
+                    boolean hasInput = scanner.hasNext();
+                    if (hasInput) {
+                        ret =  scanner.next();
+                    } else {
+                        ret = null;
+                    }
+                    return ret;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    connection.disconnect();
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            result.setText(Integer.toString(res));
+            Log.i("MainActivity","Called!");
         }
     }
 }
